c4d246dc4684261a6d7c706c2886d16fabacd40a
Ana Julia <ana.francisco4@aluno.senai.br>
2023-08-10 13:59:40 -0300

    Enviando atividade

---
+++ Fundamentos/src/aulaestruturadecisao/exemplo1.java
@@ -0,0 +1,17 @@
+
+
+package aulaestruturadecisao;
+
+
+public class exemplo1 {
+    public static void main(String[] args) {
+        int idade = 17;
+        
+        if (idade >= 18) {
+            System.out.println("Vpce pode entrar na festa");
+            
+        } else {
+            System.out.println("Voce não pode entrar na festa");
+        }
+    }
+}
---
+++ Fundamentos/src/aulaestruturadecisao/exemplo2.java
@@ -0,0 +1,20 @@
+
+
+package aulaestruturadecisao;
+
+
+public class exemplo2 {
+    public static void main(String[] args) {
+        int temperatura = 15;
+        
+        if (temperatura == 15) {
+            System.out.println("está ficando frio");
+        } else if (temperatura == 20)  {  
+            System.out.println("está ficando agradavel");
+        } else if (temperatura == 30)   {
+            System.out.println("está ficando calor");
+        
+        }
+    }
+    }
+
