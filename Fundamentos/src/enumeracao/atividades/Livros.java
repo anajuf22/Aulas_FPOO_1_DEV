fdaf598aac654ebfbcf690ca267d295935de0748
Ana Julia <ana.francisco4@aluno.senai.br>
2023-08-17 11:09:55 -0300

    Enviando atividades enum

---
+++ Fundamentos/src/enumeracao/atividades/Livros.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enumeracao.atividades;
+
+import javax.lang.model.type.TypeMirror;
+
+/**
+ *
+ * @author Aluno
+ */
+public enum Livros {
+    ROMANCE, TERROR, FICÇÃO, COMEDIA, AVENTURA
+    
+}
---
+++ Fundamentos/src/enumeracao/atividades/ProdutosCantina.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enumeracao.atividades;
+
+/**
+ *
+ * @author Aluno
+ */
+public enum ProdutosCantina {
+    SALGADO, REFRIGERANTE, SUCO, BOMBOM, BALA, CHICLETES, PIRULITO, JUJU
+    
+}
